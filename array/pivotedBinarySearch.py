a8e781fb3295ba26843fbd7bf4dfa084cb6cb353
sharmakajal0 <sharma.kajal.sitm@gmail.com>
2019-06-26 21:30:48 +0530

    Search an element in a sorted and pivoted array

---
+++ array/pivotedBinarySearch.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+def pivotedBinarySearch(arr, n, key):
+    pivot = findPivot(arr, 0, n - 1)
+    if pivot == -1:
+        return binarySearch(arr, 0, n-1, key)
+    if arr[pivot] == key:
+        return pivot
+    if arr[0] <= key:
+        return binarySearch(arr, 0, pivot - 1, key)
+    return binarySearch(arr, pivot + 1, n - 1, key)
+
+def findPivot(arr, low, high):
+    if high < low:
+        return -1
+    if high == low:
+        return low
+    mid = int((high + low)/2)
+    if mid < high and arr[mid] > arr[mid + 1]:
+        return mid
+    if mid > high and arr[mid] < arr[mid - 1]:
+        return mid - 1
+    if arr[low] >= arr[mid]:
+        return findPivot(arr, low, mid - 1)
+    return findPivot(arr, mid + 1, high)
+
+def binarySearch(arr, low, high, key):
+    if high < low:
+        return -1
+    
+    mid = int((low + high)/2)
+
+    if key == arr[mid]: 
+        return mid 
+    if key > arr[mid]: 
+        return binarySearch(arr, (mid + 1), high, key) 
+    return binarySearch(arr, low, (mid -1), key)
+
+arr1 = list(map(int, input().split()))
+n = len(arr1)
+arr1.sort()
+print(arr1)
+key = int(input())
+print(pivotedBinarySearch(arr1, n, key))
