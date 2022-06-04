ac8965973ed5b63cfd79a4f4a2c3f934827892d5
sharmakajal0 <sharma.kajal.sitm@gmail.com>
2022-06-04 22:13:10 +0530

    added linked list in java

---
+++ linked-list/LinkedList.java
@@ -0,0 +1,31 @@
+class LinkedList {
+    Node head;
+
+    static class Node {
+        int value;
+        Node next;
+
+        Node(int d){
+            value = d;
+            next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+
+        linkedList.head = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+
+        linkedList.head.next = second;
+        second.next = third;
+
+        third.next = null;
+
+        while (linkedList.head != null) {
+            System.out.println(linkedList.head.value + " ");
+            linkedList.head = linkedList.head.next;
+        }
+    }
+}
